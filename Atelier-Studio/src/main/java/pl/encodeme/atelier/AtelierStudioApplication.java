10eae3cb369c3481ec24d6dd978a45a963f87864
sas-arkadiusz <kontakt.sas.arkadiusz@gmail.com>
2020-09-22 21:33:22 +0200

    Initial commit

---
+++ Atelier-Studio/src/main/java/pl/encodeme/atelier/AtelierStudioApplication.java
@@ -0,0 +1,13 @@
+package pl.encodeme.atelier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierStudioApplication.class, args);
+	}
+
+}
---
+++ Atelier-Studio/src/test/java/pl/encodeme/atelier/AtelierStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.encodeme.atelier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
